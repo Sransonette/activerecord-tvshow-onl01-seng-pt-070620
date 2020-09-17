71150b590559cb65caa9e200f68214aa6416626f
Sean Ransonette <sransonette@gmail.com>
2020-09-17 02:25:00 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -31,10 +31,14 @@ class Show < ActiveRecord::Base
     
   end 
   
+<<<<<<< HEAD
   def self.shows_by_alphabetical_order
     Show.order(:name)
     
   end
+=======
+  
+>>>>>>> d6e5f7f6d3fbb85369202d672416b81738b95915
   
   
   
